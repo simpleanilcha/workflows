33304e78cf122306bc763a686ce5d53fe3293cc4
Anil Maharjan <simpleanilcha@gmail.com>
2015-10-07 07:36:04 +0545

    added watch to default task

--- gulpfile.js
+++ gulpfile.js
@@ -19,17 +19,17 @@ var jsSources = [
 var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function(){
-	gulp.src('coffeeSources')
+	gulp.src(coffeeSources)
 	.pipe(coffee({bare: true})
 		.on('error', gutil.log))
-	.pipe(gulp.dest('components/scripts'))
+	.pipe(gulp.dest('components/scripts'));
 });
 
 gulp.task('js', function(){
 	gulp.src(jsSources)
 	.pipe(concat('script.js'))
 	.pipe(browserify())
-	.pipe(gulp.dest('builds/development/js'))
+	.pipe(gulp.dest('builds/development/js'));
 });
 
 gulp.task('compass', function(){
@@ -40,7 +40,15 @@ gulp.task('compass', function(){
 		style: 'expanded'
 	}))
 	.on('error', gutil.log)
-	.pipe(gulp.dest('builds/development/css'))
+	.pipe(gulp.dest('builds/development/css'));
 });
 
-gulp.task('default', ['coffee', 'js', 'compass']);
+// watch task
+gulp.task('watch', function(){
+	gulp.watch(coffeeSources, ['coffee']);
+	gulp.watch(jsSources, ['js']);
+	gulp.watch('components/sass/*.scss', ['compass']);
+});
+
+// default task
+gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
